c70b2b48648ca8e4973d82d553da99d1ca0c3840
Arno Klenke <arno_klenke@outlook.com>
2026-08-29 10:58:26 +0200

    Fix duplicate items in module list

--- DependenciesWAS/DependencyModuleList.xaml.cs
+++ DependenciesWAS/DependencyModuleList.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using CommunityToolkit.WinUI.UI.Controls;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -48,6 +38,14 @@ namespace Dependencies
 
 			return hashcode;
 		}
+
+        public override bool Equals(object obj)
+        {
+			if (obj is not ModuleCacheKey otherKey)
+				return false;
+
+			return Name == otherKey.Name && Flags == otherKey.Flags && Filepath == otherKey.Filepath;
+        }
 
 		public string Name;
 		public string Filepath;
